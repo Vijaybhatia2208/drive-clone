f97bf11755a00ab124b02c899796c645e80a1660
Vijaybhatia2208 <shanubhatia123@gmail.com>
2025-06-03 08:01:04 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      Wear it like armor, and it can never be used to hurt you.
+    </main>
+  );
+}
